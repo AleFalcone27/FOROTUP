d00985c3dcd99e950fedc0fc9197c07eae9c11ec
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-04-10 12:21:04 -0300

    Create components directory

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <nav className='navabar'>
+            <h1>
+                TUSI BLOG
+            </h1>
+        </nav>
+    );
+}
+
+export default Navbar;
