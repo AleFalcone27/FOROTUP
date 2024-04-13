db489ebe2fec8bb3e064f8dd54b82995af966eff
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-04-13 01:59:13 -0300

    Conexión con el backend Done

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,22 +1,34 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Link} from 'react';
 import '../styles/LoginPage.css'
 
 const API_URL = 'http://127.0.0.1:5000';
 
-const Navbar = () => {
-
+const LogIn = () => {
     const [data, setData] = useState([{}]);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    useEffect(() => {
-        fetch(`${API_URL}/`).then(
-            res => res.json()
-        ).then(
-            data => {
-                setData(data);
-                console.log(data)
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`${API_URL}/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, password })
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data); 
+            } else {
+                console.error('Error al enviar los datos');
             }
-        )
-    },[]);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
 
     return (
         <nav className='login-page'>
@@ -26,15 +38,37 @@ const Navbar = () => {
                     <div className='login-container'>
 
                         <h2> INICIA CESIÓN </h2>
-                        <input className='input-email' type="text" placeholder='Email'/>
-                        <input className='input-pass' type="text" placeholder='Password' />
+                        <input 
+                        className='input-email' 
+                        type="text" 
+                        placeholder='Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input 
+                        className='input-pass' 
+                        type="text" 
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} 
+                         />
                         <div className='btn-container' >
-                            <input type="button" value="INGRESAR" />
-                            <input type="button" value="REGISTRARSE" />  
+                            <input 
+                            type="button" 
+                            value="INGRESAR"
+                            onClick={handleSubmit}
+                            />
+                            <a href="/register">
+                            <input 
+                                type="button" 
+                                value="REGISTRARSE"  
+                            />
+                            </a>
+     
+                             
                         </div>
                     </div>
                 </div>
-
 
                     <div className='section-aside'>
                         <h3>  </h3>
@@ -47,4 +81,4 @@ const Navbar = () => {
     );
 }
 
-export default Navbar;
+export default LogIn;
